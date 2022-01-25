f96a9455a7d40e8c91bd2793110ff568fb7a1fa8
Sumit-kj <sumitkj7.5@gmail.com>
2022-01-25 21:35:43 +0530

    [GR]activity selection

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from src import running_median
 from src import minimum_average
 from src import array_pair
+from src.Greedy import activity_selection
 
 
 if __name__ == '__main__':
+    """src solutions"""
     # running_median.solve()
-    array_pair.solve()
+    # array_pair.solve()
     # minimum_average.solve()
+
+    """Greedy Solutions"""
+    activity_selection.solve()
+
