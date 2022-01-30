246599a3cfe5921c3eea0bee6cc6adc6f31d9308
Sumit-kj <sumitkj7.5@gmail.com>
2022-01-30 21:23:39 +0530

    Number of Islands

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src import minimum_average
 from src import array_pair
 from src.Greedy import activity_selection
 from src.Greedy import kruskals_mst
+from src.Misc import number_of_islands
 
 
 if __name__ == '__main__':
@@ -13,5 +14,7 @@ if __name__ == '__main__':
 
     """Greedy Solutions"""
     # activity_selection.solve()
-    kruskals_mst.solve()
+    # kruskals_mst.solve()
 
+    """Miscellaneous"""
+    number_of_islands.solve()
