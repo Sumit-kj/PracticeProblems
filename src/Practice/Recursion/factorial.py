c90da16f6ac127e4f4adf3d1ff919655660d40f0
Sumit-kj <sumitkj7.5@gmail.com>
2026-01-04 11:15:20 +0530

    DP 101: Memo and Tab for Factorial and Fiboncci

--- src/Practice/Recursion/factorial.py
+++ src/Practice/Recursion/factorial.py
@@ -3,18 +3,9 @@ The factorial problem asks you to find the product of all positive integers from
 $$ n $$. This means you want to determine how many ways things can be arranged or multiplied in a sequence running from
 1 all the way to $$ n $$.
 
-For example, if you pick $$ n = 5 $$, the factorial of 5 (written as 5!) is the multiplication of 1 × 2 × 3 × 4 × 5.
+For example, if you pick n = 5, the factorial of 5 (written as 5!) is the multiplication of 1 × 2 × 3 × 4 × 5.
 This kind of operation is common in counting problems—like how many ways you can arrange a set of items or calculate
 permutations.
-
-The problem also involves recognizing a pattern: the factorial of a number relates to the factorial of the previous
-number multiplied by the current number, which naturally breaks down into smaller subproblems. This is foundational for
-recursive problem solving and eventually dynamic programming.
-
-Understanding factorials is key for grasping many concepts in mathematics, combinatorics, probability, and algorithms.
-
-That’s the essence of the factorial problem—finding the total product of all integers up to a given number and
-appreciating the recursive pattern within it.[2][6][7]
 """
 from src.res.Practice.Recursion.factorial import input as ip, output as op
 
@@ -26,7 +17,9 @@ def solve():
     """
     result = list()
     for n in ip.i_p:
-        result.append(factorial(n))
+        # result.append(factorial(n))
+        # result.append(factorial_memo(n))
+        result.append(factorial_tab(n))
     print(result)
 
     if result != op.o_p:
@@ -40,8 +33,43 @@ def factorial(num):
     """
     This function recursively calculates the factorial of a given number
     :param num: The number for which factorial is to be calculated
-    "return: The factyorial of the number
+    :return: The factorial of the number
     """
     if num == 0 or num == 1:
         return 1
     return num * factorial(num - 1)
+
+
+def factorial_memo(num, memo=None):
+    """
+    This function solves the factorial problem using the memoization technique
+    :param num: The number for which the factorial is to be calculated
+    :param memo: This is the memoization cache for storing the solutions to subproblems
+    :return: Factorial of the number
+    """
+    if memo is None:
+        memo = {
+            0: 1
+        }
+
+    if num in memo.keys():
+        return memo[num]
+
+    res = num * factorial_memo(num-1, memo)
+    memo[num] = res
+    return res
+
+
+def factorial_tab(num):
+    """
+    This function solves the factorial problem using tabulation technique
+    :param num: The number of which the factorial is to be calculated
+    :return: Factorial of the number
+    """
+    tab = [0] * (num + 1)
+    tab[0] = 1
+
+    for i in range(1, num + 1):
+        tab[i] = i * tab[i-1]
+
+    return tab[num]
--- src/Practice/Recursion/fibonacci.py
+++ src/Practice/Recursion/fibonacci.py
@@ -27,13 +27,16 @@ from src.res.Practice.Recursion.fibonacci import input as ip, output as op
 
 
 def solve():
+
     """
     The solution of the program
     :return: None
     """
     result = list()
     for n in ip.i_p:
-        result.append(fibonacci(n))
+        # result.append(fibonacci(n))
+        # result.append(fibonacci_mem(n))
+        result.append(fibonacci_tab(n))
     print(result)
 
     if result != op.o_p:
@@ -47,8 +50,44 @@ def fibonacci(num):
     """
     This function recursively calculates the factorial of a given number
     :param num: The number for which factorial is to be calculated
-    "return: The factyorial of the number
+    :return: The factorial of the number
     """
     if num == 0 or num == 1:
         return num
     return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+def fibonacci_mem(num, memo=None):
+    """
+    This function solves fibonacci series using memoization
+    :param num: The number for which the factorial is to be calculated
+    :param memo: The cache used for storing results of subproblem
+    :return: The factorial of the number
+    """
+    if memo is None:
+        memo = {
+            0: 0,
+            1: 1
+        }
+
+    if num not in memo.keys():
+        memo[num] = fibonacci_mem(num - 1, memo) + fibonacci_mem(num - 2, memo)
+
+    return memo[num]
+
+
+def fibonacci_tab(num):
+    """
+    This function solves fibonacci series using tabulation
+    :param num: The number for which the factorial is to be calculated
+    :return: The factorial of the number
+    """
+    if num == 0:
+        return 0
+    tab = [0] * (num + 1)
+    tab[0], tab[1] = 0, 1
+
+    for i in range(2, num + 1):
+        tab[i] = tab[i-1] + tab[i-2]
+
+    return tab[num]
